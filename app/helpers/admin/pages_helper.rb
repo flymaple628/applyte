00ca78c796a88335f15dc54cac1bdc73a3d84400
ChiuGypin <crsgypin@gmail.com>
2015-07-25 16:13:09 +0800

    admin helper

--- app/helpers/admin/pages_helper.rb
+++ app/helpers/admin/pages_helper.rb
@@ -1,2 +1,8 @@
 module Admin::PagesHelper
+
+	def setup_program(program)
+		program.build_address unless program.address
+		program
+	end
+
 end
--- app/models/alumn.rb
+++ app/models/alumn.rb
@@ -2,6 +2,6 @@ class Alumn < ActiveRecord::Base
 	belongs_to :profile
 
 	has_many :program_alumnships
-  has_many :program_alumns, :through=>:program_alumnships, :source=>:programs
+  has_many :program_alumns, :through=>:program_alumnships, :source=>:program
 
 end
